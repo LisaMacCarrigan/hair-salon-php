0344a92259ba3d8d46d020b4338b0de4e11bfa7d
Lisa MacCarriagn <lisa.maccarrigan@gmail.com>
2016-09-23 10:05:03 -0400

    set up project structure, add files for two classes, install composer

---
+++ app/app.php
@@ -0,0 +1,20 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+    $app = new Silex\Application();
+
+    $server = "mysql:host=localhost:8889;dbname=hair_salon";
+    $username = "root";
+    $password = "root";
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
+
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("index.html.twig");
+    });
+
+    return $app;
+ ?>
