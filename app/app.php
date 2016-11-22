1172b0f78f97bbf29d5889abd064f846c109ff13
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-21 22:28:31 -0500

    resolve timezone error by adding date_default_timezone_set for New York

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Client.php";
     require_once __DIR__."/../src/Stylist.php";
+    date_default_timezone_set('America/New_York');
 
     $app = new Silex\Application();
 
